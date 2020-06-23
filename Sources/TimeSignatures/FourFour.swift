350765c2933c7cfc76cca67dfbab95ce02b8ac68
Ryan Cumley <ryan@ryancumley.com>
2020-06-23 13:55:16 -0600

    Trying the infix operator style for combineLatest

--- Sources/TimeSignatures/FourFour.swift
+++ Sources/TimeSignatures/FourFour.swift
@@ -29,8 +29,8 @@ final class Signal<T>: Publisher {
     private var subscribers: Array<AnySubscriber<Event<T>, Never>> = []
     func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Event<T> == S.Input { subscribers.append(AnySubscriber(subscriber)) }
     
-    private(set) var currentValue: T?
-    private(set) var previousValue: T?
+    fileprivate(set) var currentValue: T?
+    fileprivate(set) var previousValue: T?
     func update(_ newValue: T) {
         let event = Event<T>(new: newValue, previous: currentValue)
         
@@ -50,6 +50,8 @@ final class Signal<T>: Publisher {
         self.currentValue = current
         self.previousValue = previous
     }
+    
+    
     
     deinit {
         subscribers.forEach{ $0.receive(completion: .finished) }
@@ -89,6 +91,13 @@ protocol ReactiveRenderer {
 precedencegroup ReactiveStreamPrecedence {
     lowerThan: TernaryPrecedence
     higherThan: AssignmentPrecedence
+    associativity: left
+    assignment: false
+}
+
+precedencegroup CombineReactiveStreamPrecedence {
+    lowerThan: TernaryPrecedence
+    higherThan: ReactiveStreamPrecedence
     associativity: left
     assignment: false
 }
@@ -136,13 +145,87 @@ func ~>> <S,C: ReactiveRenderer>(lhs: Signal<S>, rhs: C) -> C where S == C.Upstr
     return rhs
 }
 
+infix operator <~>: CombineReactiveStreamPrecedence
 
+func <~><J: ReactiveComponent, K: ReactiveComponent>(first: J, second: K) -> Signal<(J.Model?, K.Model?)> {
 
-//func combineStreams<J: ReactiveComponent, K: ReactiveComponent>(first: J, second: K) -> Signal<(J.Model, K.Model)> {
-//
-//    ///current & previous model snipping needs to happen here. Possible combinatorial explosion of arguments and cases, let's think about it a bit in the abstract and in what a user would expect to happen. Especially given that users would be justified to expect combining streams together in various states of initial/flowing status. ie. joining two un-initialized streams together, or two already flowing ones, or mixtures; up to the airity of combine I'm going to support.
-//
-//    let current = (first.state.currentValue, second.state.currentValue)
-//    let previous = (first.state.previousValue, second.state.previousValue)
-//    return Signal<(J.Model, K.Model)>(current: current, previous: previous)
-//}
+    ///current & previous model snipping needs to happen here. Possible combinatorial explosion of arguments and cases, let's think about it a bit in the abstract and in what a user would expect to happen. Especially given that users would be justified to expect combining streams together in various states of initial/flowing status. ie. joining two un-initialized streams together, or two already flowing ones, or mixtures; up to the airity of combine I'm going to support.
+    
+    //So as a consumer, what do these tupple values look like for me?
+    //Signal's can have nil current & previous values, but always emit Event's with at least the currentValue populated.
+    //So my typeChecking problem here is that arbitrary elements of the tupple may be nil, or have a value
+    
+    //Do I punt this on to the caller by making them treat each element of the tupple as optional?
+
+    let current = (first.state.currentValue, second.state.currentValue)
+    let previous = (first.state.previousValue, second.state.previousValue)
+    let signal = Signal<(J.Model?, K.Model?)>(current: current, previous: previous)
+    
+    //and now we need to CombineLatest to fire our new Signal
+    
+    return signal
+    //So does the unsatisfying feeling of this derive from the structural reality of combining streams? It is completely valid that one of the upstream components may be initialized with no values, and never emit one, and we'd still want the combineLatest to fire those values.
+    
+    
+    //(<nil,nil>, <nil,nil>)
+    //(<v, nil>, <nil,nil>)
+    //(<t, v>, <nil,nil>)
+    //(<v, nil>, <w,x>)
+    //These are all totally valid upstream states of a component which is collecting multiple streams.
+    //And we're not stuck in an optional world now, because the combining component publishes any kind of model it wants, only when it wants to. So the nils can be ignored/swallowed if that's the right behavior.
+}
+
+struct UserService: ReactiveComponent {
+    typealias UpstreamModel = ()
+    enum Model { case initial, data }
+    var state = Signal<Model>()
+
+}
+struct SelectedRecipeService: ReactiveComponent {
+    typealias UpstreamModel = ()
+    enum Model { case initial, meat }
+    var state = Signal<Model>()
+}
+
+struct CombinedStreams: ReactiveComponent {
+    enum Model { case one }
+    var state = Signal<Model>()
+    typealias UpstreamModel = (UserService.Model?, SelectedRecipeService.Model?)
+    
+    func react(toNew: (UserService.Model?, SelectedRecipeService.Model?)) {
+        switch toNew {
+            
+            //If you're sure that one of the `nil` states won't ever happen, you can just ignore that case/pattern match
+            //So the practical implication of these optionals doesn't even need to be unwrapped directly, and you as an author have great freedom to write tersely if you like, or explicitly cover all possible upstream states.
+            //That's probably reasonable to expect you to be aware of what's possible when combining upstream streams, so it's ok if we're assuming you'll be responsible here.
+            
+        case (.data, nil):
+            
+            break
+            
+        case (nil, nil):
+            break
+            
+        case (nil, .meat):
+            
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    func react(toNew: (UserService.Model?, SelectedRecipeService.Model?), withPrevious: (UserService.Model?, SelectedRecipeService.Model?)) {
+        
+    }
+}
+
+let user = UserService()
+let recipe = SelectedRecipeService()
+let combined = CombinedStreams()
+//let stream = combineStreams(first: user, second: recipe) ~>> combined
+let stream = user <~> (recipe /* imagine some kind of longer stream definition in here  */) ~>> combined
+
+//So let's talk about the use of a function or an operator here, it definitely breaks up the visual flow of ~>>, and would nest more complex earlier streams inwardly; ie. not read as cleanly flowing from left to right.
+
+//If we tried infix with higher precedence, could we force you to use parens for clear order of operations?
