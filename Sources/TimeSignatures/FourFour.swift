1200a1c8bc3cb93f9a5c44b2775f3c4f9e4c85c7
Ryan Cumley <ryancumley@gmail.com>
2020-07-14 09:19:26 -0600

    Reworking my earlier Redux implementation to use the new signatures, as well as the new 'Graph' based approach of modelling appState as graphs and subgraphs

--- Sources/TimeSignatures/FourFour.swift
+++ Sources/TimeSignatures/FourFour.swift
@@ -10,7 +10,7 @@ import Combine
 ///Since this abstraction is still a singular value, we may still exercise all of our familiar Functional Programming toolkits to ensure that every Subscribing Component processes a `well-ordered` sequence of discreet events.
 ///
 ///However, a clever author could certainly compare what the publisher intended as `previous` with how the events actually arrived over the asynchronous boundary, and construct a valid alternate history/sequence/stream, allowing them to "break" the core abstraction of reactive programming: locally well-ordered sequences. In practice, this is unlikely to cause real problems.
-struct Event<T> {
+public struct Event<T> {
     let new: T
     let previous: T?
 }
@@ -22,16 +22,16 @@ struct Event<T> {
 ///As a Publisher, Signal<T> will emit values every time `update(_ value)` is called, regardless of whether or not it was initialized with one or more values.
 ///
 ///Whether or not you think it's cheating to "remember" a portion of a stream's history (in this case merely the most recently emitted value alone), this abstraction still allows for Functional Programming purity, as the `Event<T>` emitted is a pure value type, eminently suitable for pure functional computation.
-final class Signal<T>: Publisher {
-    typealias Output = Event<T>
-    typealias Failure = Never
+public final class Signal<T>: Publisher {
+    public typealias Output = Event<T>
+    public typealias Failure = Never
     
     private var subscribers: Array<AnySubscriber<Event<T>, Never>> = []
-    func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Event<T> == S.Input { subscribers.append(AnySubscriber(subscriber)) }
+    public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Event<T> == S.Input { subscribers.append(AnySubscriber(subscriber)) }
     
     fileprivate(set) var currentValue: T?
     fileprivate(set) var previousValue: T?
-    func update(_ newValue: T) {
+    public func update(_ newValue: T) {
         let event = Event<T>(new: newValue, previous: currentValue)
         
         subscribers.enumerated().forEach{
@@ -46,12 +46,10 @@ final class Signal<T>: Publisher {
         self.currentValue = newValue
     }
     
-    init(current: T? = nil, previous: T? = nil) {
+    public init(current: T? = nil, previous: T? = nil) {
         self.currentValue = current
         self.previousValue = previous
     }
-    
-    
     
     deinit {
         subscribers.forEach{ $0.receive(completion: .finished) }
@@ -134,6 +132,10 @@ protocol ReactiveComponent {
     func react(toNew: UpstreamModel, withPrevious: UpstreamModel)
 }
 
+extension ReactiveComponent {
+    func react(toNew: UpstreamModel, withPrevious: UpstreamModel) {}
+}
+
 extension ReactiveComponent where UpstreamModel == () {
     func react(toNew: UpstreamModel) {}
     func react(toNew: UpstreamModel, withPrevious: UpstreamModel) {}
@@ -146,6 +148,10 @@ protocol ReactiveRenderer {
     associatedtype UpstreamModel
     func react(toNew: UpstreamModel)
     func react(toNew: UpstreamModel, withPrevious: UpstreamModel)
+}
+
+extension ReactiveRenderer {
+    func react(toNew: UpstreamModel, withPrevious: UpstreamModel) {}
 }
 
 precedencegroup ReactiveStreamPrecedence {
@@ -297,3 +303,92 @@ let stream = user <~> (recipe /* imagine some kind of longer stream definition i
 //So let's talk about the use of a function or an operator here, it definitely breaks up the visual flow of ~>>, and would nest more complex earlier streams inwardly; ie. not read as cleanly flowing from left to right.
 
 //If we tried infix with higher precedence, could we force you to use parens for clear order of operations?
+
+
+
+
+struct Edge<P,S>: Hashable, Equatable {
+    static func == (lhs: Edge<P, S>, rhs: Edge<P, S>) -> Bool {
+        return lhs.publisher == rhs.publisher && lhs.subscriber == rhs.subscriber
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(String(describing: publisher))
+        hasher.combine(String(describing: subscriber))
+    }
+    
+    let publisher: P.Type
+    let subscriber: S.Type
+    //In practice, we're going to want to allow multi-cast or mirrored graphs giving us multiple Edge instances of the same type.
+    //But we want Store observers to register closures based solely on the Type of the Edge
+    //This would mean internally keeping hooks on each instance so each edge can be effectively removed from the Store upon teardown of it's sub-graph, so then Edge identity will have to become a compound (Edge.Type, instanceIdentity) like thing, and unless we want to pass uuid's around the API, the instanceIdentity should be derivable post-hoc
+}
+
+
+
+
+//MARK:- Redux
+
+//Let's start with the idea of the global store being a static caseless enum which may be configured/populated with an existential who supplies the needed hooks
+public protocol Bijection {
+    associatedtype T
+    var arrow: (T) -> Void { get }
+}
+
+//public struct AnyArrow<T>: Bijection {
+//    typealias Element = T
+//    public let arrow: (Element) -> Void
+//    init(_ arrow: ) { self.arrow = arrow.arrow }
+//}
+
+public protocol UniversalArrow { //Let's start with our metaphor and name semantics from Yoneda, and work into something more approachable as it emerges
+    //I need to supply you with a collection of pairs of ( T.Type, @escaping (T) -> Void)
+    var arrows: String { get } //already into the hetergeneous type hole again, are we?
+    
+}
+
+public protocol GlobalDataFlowStateLens {
+    var liveObservations: Set<AnyCancellable> { get set }
+//    var liveObservations: [Edge<Any,Any>: [(Any) -> Void]]
+}
+
+fileprivate struct LensableIdentity: GlobalDataFlowStateLens {
+    var liveObservations = Set<AnyCancellable>()
+}
+
+public enum Redux {
+    private static var lens: GlobalDataFlowStateLens = LensableIdentity() // <-- evaluate later if we should load this type as nil, or if there's a sensible `zero` i can originate it with instead and save some unwrapping later
+    
+    public static func applyLens(_ lens: GlobalDataFlowStateLens) {
+        //invoke shutdown work on existing lens existential
+        
+        Redux.lens = lens
+        
+        //invoke any startup work on the new lens existential
+    }
+    
+    public static func registerDownstreamNode<T>(_ node: inout Signal<T>){ // <-- evaluate later if we need to wrap each "hot" signal with metadata, or if we can infer everything we need from the meta-typing of T and the machinery of Signal<T>
+        
+        node.sink(receiveCompletion: { completion in
+            //here we need to find and remove this observation from the liveObservations pool
+            //and do any other cleanup work like notifying meta-observers if appropriate
+        }){
+            value in
+            
+            
+        }.store(in: &lens.liveObservations)
+    }
+}
+
+enum Store {
+    static var liveObservations: [Edge<Any,Any>: [(Any) -> Void]] = [:]
+    
+    static func observer<P,S: ReactiveComponent, R>(for edge: Edge<P,S>) -> ((S.Model) -> Void)? where S.Model == R {
+        if let match = liveObservations.first(where: { element in element.key.publisher is P && element.key.subscriber is S }) {
+            return { value in match.value.forEach{ $0(value) } }
+        }
+        return nil
+    }
+}
+
+//Let's prove with an example that the typechecking here survives the Any conversions. Probably time for a Unit test target.
